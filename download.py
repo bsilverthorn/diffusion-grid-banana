e87dbc96ffd6b66db3faf87c0a709cddc5b469ca
Bryan Silverthorn <bsilverthorn@gmail.com>
2022-11-26 23:09:35 +0000

    download.py: fetch the correct model.
    ... instead of the of BERT model used in the banana.dev template.

--- download.py
+++ download.py
@@ -1,13 +1,9 @@
-# In this file, we define download_model
-# It runs during container build time to get model weights built into the container
+import os
 
-# In this example: A Huggingface BERT model
-
-from transformers import pipeline
-
-def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+from runner import init_model
 
 if __name__ == "__main__":
-    download_model()
+    init_model(
+        hf_auth_token=os.environ["HF_AUTH_TOKEN"],
+        torch_device="cpu",
+    )
